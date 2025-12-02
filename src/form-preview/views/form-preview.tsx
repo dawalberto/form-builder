e2e9a6ec9163891eb2d56709a2a2a881449a5e13
dawalberto <albertoinformatica97@gmail.com>
2025-12-02 16:23:45 +0100

    refactor: remove CopyToClipboardButton component and its usage in FormPreview
    - Deleted the CopyToClipboardButton component from the codebase.
    - Removed its export from the components index file.
    - Updated FormPreview to use Sandpack for displaying the generated form component.

--- src/form-preview/views/form-preview.tsx
+++ src/form-preview/views/form-preview.tsx
@@ -1,25 +1,38 @@
+import { Sandpack } from "@codesandbox/sandpack-react"
 import type { TFormSchemaType } from "@/schema-builder/models"
-import { CopyToClipboardButton } from "../../components/copy-to-clipboard-button"
 import { useFormPreview } from "../hooks"
-
 export const FormPreview = ({ schema }: { schema: TFormSchemaType }) => {
   const { formStringComponent } = useFormPreview({ schema })
 
   return (
     <div>
       <h2>Generated Form Component</h2>
-      <div className="relative">
-        <CopyToClipboardButton
-          text={formStringComponent}
-          ariaLabel="Copy generated form component code"
-          className="absolute right-2 top-10"
+      {formStringComponent && (
+        <Sandpack
+          files={{
+            "/form.tsx": formStringComponent,
+            "/App.tsx": `
+            import React from "react";
+            import { ${schema.name}Form } from "./form";
+
+            export default function App() {
+              return <${schema.name}Form />
+            }`,
+          }}
+          theme="auto"
+          template="react-ts"
+          options={{
+            externalResources: ["https://cdn.jsdelivr.net/npm/sakura.css/css/sakura.css"],
+            readOnly: true,
+            showReadOnly: false,
+            showNavigator: true,
+            editorHeight: "100dvh",
+            activeFile: "/form.tsx",
+            visibleFiles: ["/form.tsx"],
+            editorWidthPercentage: 70,
+          }}
         />
-        <code>
-          <pre className="overflow-x-auto p-4 bg-stone-900 text-stone-100 rounded-md">
-            {formStringComponent}
-          </pre>
-        </code>
-      </div>
+      )}
     </div>
   )
 }
